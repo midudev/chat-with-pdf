2be9fe724dcbdf560f0f6157252823378ad258d5
Oscar Recio <osrecio@gmail.com>
2024-02-03 18:52:41 +0100

    Set AppStatus to init

--- src/store.ts
+++ src/store.ts
@@ -7,7 +7,7 @@ export const APP_STATUS = {
   ERROR: -1
 }
 
-export const appStatus = writable(APP_STATUS.CHAT_MODE)
+export const appStatus = writable(APP_STATUS.INIT)
 export const appStatusInfo = writable({
   id: 'c1a098ffcb49079c8180b18c7b15229a',
   url: 'https://res.cloudinary.com/midudev/image/upload/v1706810969/pdf/khiice5vqnr1gcn1pmtq.pdf',
